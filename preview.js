8a533112937d09c0cae5a9bf9c51dabced64f682
SACPA <williamjoel1909@gmail.com>
2024-11-28 12:22:16 -0500

    reload_detect

--- preview.js
+++ preview.js
@@ -2,6 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const urlParams = new URLSearchParams(window.location.search);
     const audioFile = urlParams.get("audio");
   
+    window.history.pushState(null, null, window.location.href); // Evita que se retroceda
+    window.onpopstate = function () {
+      // Al detectar retroceso, recarga la página
+      location.reload();
+    };
+    
+
     // Si no hay archivo de audio, redirige al usuario a la página principal
     if (!audioFile) {
       window.location.href = "index.html";
